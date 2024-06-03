0a3b3cd1165eb9ca6bfc111c68d9c3ad42812549
James Lu <jamesleu923@gmail.com>
2024-06-03 16:14:08 +0800

    Add files via upload

---
+++ App_Code/Get_DataSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+/// <summary>
+/// MyDataSet 的摘要描述
+/// </summary>
+public class Get_DataSet
+{
+    private string cn;
+    private string sqlcmd;
+    private string TableName;
+
+    public Get_DataSet(string Tname)
+    {
+        this.cn = System.Configuration.ConfigurationManager.ConnectionStrings["MIS_3"].ConnectionString;
+        this.TableName = Tname;
+    }
+    public Get_DataSet(string user_sqlcmd,string Tname)
+    {
+        this.cn=System.Configuration.ConfigurationManager.ConnectionStrings["MIS_3"].ConnectionString;
+        this.sqlcmd = user_sqlcmd;
+        this.TableName = Tname;
+    }
+    public DataSet SelectDB()
+    {
+        SqlDataAdapter MyAdapter = new SqlDataAdapter(this.sqlcmd, this.cn);
+        DataSet vDataSet=new DataSet();
+        try {
+            MyAdapter.Fill(vDataSet, this.TableName);
+        } catch (Exception)
+        {
+            // error message
+
+        }
+        return vDataSet;
+    }
+}
---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_20190522.Startup))]
+namespace _20190522
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
